85022bd95162782ed637027aa30a6224a90ad242
Oteizam03 <oteizam@udesa.edu.ar>
2026-06-01 14:22:06 -0300

    cree los archivos

---
+++ :src/filtrado.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  1 14:18:55 2026
+
+@author: milagrosoteiza
+"""
+
+e
---
+++ :src/validaciones.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  1 14:18:40 2026
+
+@author: milagrosoteiza
+"""
+
+d
