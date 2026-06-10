00e374edfe281809479618d611f9e75c33dbd17c
JustoBarbosa <jbarbosamoreira@udesa.edu.ar>
2026-06-10 15:42:19 -0300

    update filtrado.py

--- src/filtrado.py
+++ src/filtrado.py
@@ -1,14 +1,22 @@
 import pandas as pd 
-"""
-df = pd.read_excel("dataframe.xlsx", dtype = {"id" : str, "sexo": str,
-                                              "nombre": str, "apellido": str, "carrera": str, 
-                                              "zona por la que vive": str, "hobbies": str,
-                                              "estilo musical favorito": str, "edad" : int, "altura": int,
-                                              "sexualidad": str, "instagram": str, "telefono": str})
-"""
+
 
 def filtrar_usuarios(df,genero, sexualidad, edad_min, edad_max, altura_min, id_usuario): 
-    
+    """
+            Filtra el DataFrame eliminando perfiles incompatibles con el usuario.
+            
+            parametros:
+                df (DataFrame): dataset completo de usuarios 
+                genero (str): sexo del usuario ("m" o "f")
+                sexualidad (str): orientacion del usuario ("heterosexual", "homosexual", "bisexual")
+                edad_min (int): edad mínima aceptada para el match (aclarado por el usuario)
+                edad_max(int): edad máxima aceptada para el match
+                altura_min(int): altura maxima aceptada para el match (en cm)
+                id_usuario (str): legajo del usuario activo, para excluirse a sí mismo
+                
+            returns:
+                candidatos (dataframe): subconjunto de candidatos que pasan los filtros duros
+    """
     candidatos = df[df["id"] != id_usuario].copy()# esto fue con chat
 
     candidatos = candidatos[
@@ -22,7 +30,7 @@ def filtrar_usuarios(df,genero, sexualidad, edad_min, edad_max, altura_min, id_u
     elif sexualidad == "heterosexual":
         candidatos = candidatos[((candidatos["sexualidad"] == "heterosexual") | (candidatos["sexualidad"] == "bisexual")) & (candidatos["sexo"] != genero)]
     else:
-        candidatos = candidatos[((candidatos["sexualidad"] == "heterosexual") & (candidatos["sexo"] != genero)) | (candidatos["sexualidad"] == "bisexual") | ((candidatos["sexualidad"] == "homosexual") & (candidatos["sexo"] == genero))]
+        candidatos = candidatos[(((candidatos["sexualidad"] == "homosexual") | (candidatos["sexualidad"] == "bisexual")) & (candidatos["sexo"] == genero)) | (((candidatos["sexualidad"] == "heterosexual") | (candidatos["sexualidad"] == "bisexual")) & (candidatos["sexo"] != genero))]
         
         
     
